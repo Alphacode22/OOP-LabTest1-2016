d405aa013653d5cd61d8d1d00bc854513aa012e9
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2021-04-18 18:41:36 +0100

    printStars

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,7 +1,37 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet{
-    
+    ArrayList<Star> stars = new ArrayList<Star>();
+
+    @Override
+    public void settings() {
+        // TODO Auto-generated method stub
+        super.settings();
+    }
+
+    @Override
+    public void setup() {
+        // TODO Auto-generated method stub
+        super.setup();
+        loadData();
+    }
+
+    public void loadData(){
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for(TableRow row: table.rows())
+        {
+            Star s = new Star(row);
+            stars.add(s);
+        }
+    }
+
+
+
+
 }
